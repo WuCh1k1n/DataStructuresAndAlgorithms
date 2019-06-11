23b523802dc1a1eeed86b175671ee91ffae7e2d5
wuch1k1n <wuch1k1n@qq.com>
2019-06-11 21:15:55 +0800

    feat: add LinkedBag

---
+++ linkedbag.py
@@ -0,0 +1,91 @@
+from node import Node
+
+class LinkedBag(self):
+    """A link-based bag implementation."""
+
+    # Constructor
+    def __init__(self, sourceCollection=None):
+        """Sets the initial state of self, which includes the
+        content of sourceCollection, if it's present."""
+        self._items = None
+        self._size = 0
+        if sourceCollection:
+            for item in sourceCollection:
+                self.add(item)
+
+    # Accessor methods
+    def isEmpty(self):
+        """Returns True if len(self) == 0, or False otherwise."""
+        return len(self) == 0
+
+    def __len__(self):
+        """Returns the number of items in self."""
+        return self._size
+
+    def __iter__(self):
+        """Supports iteration over a view of self."""
+        cursor = self._items
+        while not cursor is None:
+            yield cursor.data
+            cursor = cursor.next
+
+    def __str__(self):
+        """Return the string representation of self."""
+        return "{" + ",".join(map(self)) + "}"
+
+    def __add__(self, other):
+        """Returns a new bag containing the contents
+        of self and other."""
+        result = ArrayBag(self)
+        for item in other:
+            result.add(item)
+        return result
+
+    def __eq__(self, other):
+        """Returns True if self equals other,
+        or False otherwise."""
+        if self is other: return True
+        if type(self) != type(other) or len(self) != len(other): return False
+        for item in other:
+            if not item in self:
+                return False
+        return True
+
+    # Mutator methods
+    def clear(self):
+        """Make self become empty."""
+        self._size = 0
+        self._items = Array(ArrayBag.DEFAULT_CAPACITY)
+
+    def add(self, item):
+        """Add item to self."""
+        # Check array memory here and increase it if necessary
+        self._items = Node(item, self._items)
+        self._size += 1
+
+    def remove(self, item):
+        """Precondition: item is in self.
+        Raises: KeyError if item in not in self.
+        Postcondition: item is removed from self."""
+        # Check precondition and raise if necessary
+        if not item in self:
+            raise KeyError(str(item) + " not in bag")
+        # Search for the node containing the target item
+        # probe will point to the target node, and trailer
+        # will point to the one before it, if it exists
+        probe = self._items
+        trailer = None
+        for targetItem in self:
+            if targetItem == item:
+                break
+            trailer = probe
+            probe = probe.next
+        
+        # Unhook the node to be deleted, either the first one or the
+        # one thereafter
+        if probe == self._items:
+            self._items = self._items.next
+        else:
+            trailer.next = probe.next
+        # Decrement logical size
+        self._size -= 1
