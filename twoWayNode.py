54b1e2a74f7e5fa3af02bf9741e6a323c78f3708
wuch1k1n <wuch1k1n@qq.com>
2019-06-22 16:57:45 +0800

    style: Renamed twoWayNode.py to twowaynode.py

--- twoWayNode.py
+++
@@ -1,7 +0,0 @@
-class TwoWayNode(object):
-
-    def __init__(self, data, previous=None, next=None):
-        """Instantiates a TwoWayNode."""
-        self.data = data
-        self.previous = previous
-        self.next = next
